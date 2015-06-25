2aaa76bb8957ec18a6429dde0a40eac21244afc6
Florian Haas <florian@hastexo.com>
2015-06-25 17:29:39 +0200

    Stop messing around with yum repos
    The puppet-ceph module configures these automagically.

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,49 +21,8 @@ class iscsirbdhacluster ($authkey_source,
 
   include ceph::profile::client
 
-  tidy { '/etc/yum.repos.d':
-    matches => ['CentOS-*.repo'],
-    recurse => true,
-  }
-  yumrepo { 'centos-base':
-    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
-    gpgcheck => 1,
-    gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    exclude => 'ceph* python-rados* python-rbd* python-cephfs* python-ceph-compat* libcephfs* librados2* librbd1',
-    require => Tidy['/etc/yum.repos.d'],
-  }
-  yumrepo { 'centos-updates':
-    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
-    gpgcheck => 1,
-    gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
-    exclude => 'ceph* python-rados* python-rbd* python-cephfs* python-ceph-compat* libcephfs* librados2* librbd1',
-    require => Tidy['/etc/yum.repos.d'],
-  }
-  yumrepo { 'ceph':
-    baseurl  => 'http://ceph.com/rpm-firefly/rhel7/$basearch',
-    descr    => 'Ceph packages for $basearch',
-    enabled  => 1,
-    gpgcheck => 1,
-    gpgkey   => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
-  }
-  yumrepo { 'ceph-noarch':
-    baseurl  => 'http://ceph.com/rpm-firefly/rhel7/noarch',
-    descr    => 'Ceph packages for noarch',
-    enabled  => 1,
-    gpgcheck => 1,
-    gpgkey   => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
-  }
-  yumrepo { 'epel':
-    baseurl  => 'http://download.fedoraproject.org/pub/epel/7/x86_64',
-    descr    => 'EPEL',
-    enabled  => 1,
-    gpgcheck => 1,
-    exclude => 'ceph* python-rados* python-rbd* python-cephfs* python-ceph-compat* libcephfs* librados2* librbd1',
-    gpgkey   => 'https://getfedora.org/static/352C64E5.txt',
-  }
   package { 'targetcli':
     ensure  => latest,
-    require => [ Yumrepo['centos-base', 'centos-updates'] ],
   }
   class { 'corosync':
     enable_secauth    => true,
@@ -71,14 +30,11 @@ class iscsirbdhacluster ($authkey_source,
     authkey           => $authkey,
     multicast_address => $multicast_address,
     quorum_members    => $quorum_members,
-    require => [ Yumrepo['centos-base', 'centos-updates'] ]
   }
   service { 'pacemaker':
     ensure => running,
     require => Class['corosync'],
   }
-
-  Yumrepo['ceph','ceph-noarch'] -> Class['::ceph']
 
   Service['pacemaker'] -> Cs_location<| |>
   Service['pacemaker'] -> Cs_property<| |>
