d511548a2b4eb8025b33c88d74a39842d56de68c
Florian Haas <florian@hastexo.com>
2015-07-08 14:43:36 +0200

    Add Ceph cluster configuration

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,11 @@ node puppetmaster {
   include puppetmaster
 }
 
+node /^ceph\d+$/ {
+  include ceph::profile::mon
+  include ceph::profile::osd
+}
+
 node /^iscsi\d+$/ {
   include iscsirbdhacluster
 }
